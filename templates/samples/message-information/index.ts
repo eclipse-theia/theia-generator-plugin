da8f318d763e4206b13aee563f1216f01c3a6c77
Florent BENOIT <fbenoit@redhat.com>
2018-10-10 17:12:12 +0200

    usage of pluginContext to append disposables
    Change-Id: I98e05cc3e58706ebc265858840c177bf1ec2e401
    Signed-off-by: Florent BENOIT <fbenoit@redhat.com>

--- templates/samples/message-information/index.ts
+++ templates/samples/message-information/index.ts
@@ -1,9 +1,7 @@
 <%- include('../../base/ts-header.ts'); %>
 import * as theia from '@theia/plugin';
 
-const disposables: theia.Disposable[] = [];
-
-export function start() {
+export function start(context: theia.PluginContext) {
     const informationCommand: theia.Command = {
         id: 'information-message-command',
         label: 'information-message-command',
@@ -13,26 +11,20 @@ export function start() {
         label: 'Information Modal Message',
     };
 
-    disposables.push(
+    context.subscriptions.push(
         theia.commands.registerCommand(informationCommand, async (...args: any[]) => {
             const action = await theia.window.showInformationMessage('Information message!');
             theia.window.showInformationMessage('Close information message ' + action!);
         })
     );
 
-    disposables.push(theia.commands.registerCommand(informationModalCommand, async (...args: any[]) => {
+    context.subscriptions.push((theia.commands.registerCommand(informationModalCommand, async (...args: any[]) => {
             const action = await theia.window.showInformationMessage('Information modal message!', { modal: true },
                 { title: 'action1' }, { title: 'action2', isCloseAffordance: true }, { title: 'action3' });
                 theia.window.showInformationMessage('Resolve ' + action!.title);
         })
-    );
+    ));
 }
 
 export function stop() {
-    while (disposables.length) {
-        const disposable = disposables.pop();
-        if (disposable) {
-            disposable.dispose();
-        }
-    }
 }
